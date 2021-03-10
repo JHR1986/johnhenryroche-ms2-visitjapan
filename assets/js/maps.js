e2097519bcb84dbd165acda4f50dee6103502c94
JHR1986 <jhroche1986@gmail.com>
2021-03-10 12:24:49 +0000

    Insert JavaScript code relating to the map

---
+++ assets/js/maps.js
@@ -0,0 +1,97 @@
+/*jshint esversion: 6 */
+
+/*Start of Javascript Map API code */
+var map;
+var InforObj = [];
+var centerCords = {
+
+    lat: 38.2682,
+    lng: 140.8694
+};
+var markersOnMap = [{
+    placeName: "Tokyo",
+    text: "The de facto capital and most populous prefecture of Japan, with a population of approximately 37m. Tokyo is the political and economic centre of the country, as well as the seat of the Emperor of Japan and the national government.",
+    LatLng: [{
+        lat: 35.6762,
+        lng: 139.6503
+    }]
+},
+{
+    placeName: "Kyoto",
+    text: "Kyoto is a city on the island of Honshu, famous for its numerous classical Buddhist temples, gardens, imperial palaces, Shinto shrines and traditional wooden houses",
+    LatLng: [{
+        lat: 35.0116,
+        lng: 135.7681
+    }]
+},
+{
+    placeName: "Osaka",
+    text: "Osaka is a large port city and commercial center on the Japanese island of Honshu. It's known for its modern architecture, nightlife and hearty street food.",
+    LatLng: [{
+        lat: 34.6937,
+        lng: 135.5023
+    }]
+},
+{
+    placeName: "Sapporo",
+    text: "Sapporo, capital of the mountainous northern Japanese island of Hokkaido, is famous for its beer, skiing and annual Sapporo Snow Festival featuring enormous ice sculptures.",
+    LatLng: [{
+        lat: 43.0618,
+        lng: 141.3545
+    }]
+},
+];
+
+window.onload = function () {
+    initMap();
+};
+
+function addMarkerInfo() {
+    for (var i = 0; i < markersOnMap.length; i++) {
+        var contentString = '<div id="content"><h2>' + markersOnMap[i].placeName +
+            '</h2><p>' + markersOnMap[i].text + '</p></div>';
+
+        const marker = new google.maps.Marker({
+            position: markersOnMap[i].LatLng[0],
+            map: map
+        });
+
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
+
+        marker.addListener('click', function () {
+            closeOtherInfo();
+            infowindow.open(marker.get('map'), marker);
+            InforObj[0] = infowindow;
+        });
+        // marker.addListener('mouseover', function () {
+        //     closeOtherInfo();
+        //     infowindow.open(marker.get('map'), marker);
+        //     InforObj[0] = infowindow;
+        // });
+        // marker.addListener('mouseout', function () {
+        //     closeOtherInfo();
+        //     infowindow.close();
+        //     InforObj[0] = infowindow;
+        // });
+    }
+}
+
+function closeOtherInfo() {
+    if (InforObj.length > 0) {
+        InforObj[0].set("marker", null);
+        InforObj[0].close();
+        InforObj.length = 0;
+    }
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4.5,
+        center: centerCords
+    });
+    addMarkerInfo();
+}
+/*End of Javascript Map API code */
