c304a94fee1edc086560410b4ae32d22c27793d5
JHR1986 <jhroche1986@gmail.com>
2021-03-12 09:22:25 +0000

    Include code comments into sendEmail.js page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,19 +1,19 @@
 /*Start of EmailJS code */
 
 function sendMail(contactForm) {
-    emailjs.send("service_xh06r2y","visitjapan", {
+    emailjs.send("service_xh06r2y", "visitjapan", {
         "from_name": contactForm.inputName.value,
         "to_email": contactForm.inputEmail4.value,
         "information_request": contactForm.inputQuery.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
     return false;  // To block from loading a new page
 }
 
